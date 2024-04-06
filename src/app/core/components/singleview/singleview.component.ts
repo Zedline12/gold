2b7c8a2c4ea1a6865d43c9018d92a328579c22cf
Zedline12 <bedomohamed307@gmail.com>
2024-04-06 19:44:38 +0200

    singleview

---
+++ src/app/core/components/singleview/singleview.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+export enum tables{
+  pastdaysprice,
+  pricetoday,
+  golderat24 
+}
+@Component({
+  selector: 'app-singleview',
+  templateUrl: './singleview.component.html',
+  styleUrl: './singleview.component.scss'
+})
+export class SingleviewComponent implements OnInit {
+  
+  table!:tables
+    constructor(private route:ActivatedRoute){
+
+    }
+    ngOnInit(): void {
+      window.scroll(0,0);
+      const table=this.route.snapshot.queryParamMap.get("table")
+      switch(table){
+        case "pastdaysprice" :
+          this.table=tables.pastdaysprice
+           break;
+           case "pricetoday" :
+            this.table=tables.pricetoday
+            break;
+            case "golderat24" :
+              this.table=tables.golderat24
+              break;
+              default:
+      }
+      
+    }
+}
