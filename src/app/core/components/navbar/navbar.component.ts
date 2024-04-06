640d57a4ed3fe8657947ddaeebc5cd58fb29ade9
Zedline12 <bedomohamed307@gmail.com>
2024-04-06 19:44:51 +0200

    navbar edit

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TableService } from '../../services/tables.service';
 import { table } from '../../models/table';
+import { BlogService } from '../../services/blog.service';
+import { blog } from '../../models/blog';
 
 @Component({
   selector: 'app-navbar',
@@ -9,14 +11,19 @@ import { table } from '../../models/table';
 })
 export class NavbarComponent implements OnInit {
   sar!:table
+  blogs!:blog[]
   comission!:{id:number,new:number,karat:string}[]
-  constructor(private tableserv:TableService){
+  constructor(private tableserv:TableService,private blogserv:BlogService){
 
   }
   const(karat:string){
     return Number(this.comission.find(x=>x.karat==karat)?.new)
    }
   ngOnInit(): void {  
+    this.blogserv.getblogs().subscribe(x=>{
+      this.blogs=x
+      console.log(x)
+    })
       this.tableserv.getsar().subscribe(x=>{
         this.sar=x
         this.sar.karat_24=+this.sar.karat_24
